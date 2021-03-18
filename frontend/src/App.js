0853556fc7b25b86d5a47b309203e744b213eb05
wasalas <wasalas893@gmail.com>
2021-03-18 12:20:04 +0530

    create frontend project in react

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from '../src/componets/layout/header';
+import Footer from '../src/componets/layout/Footer';
+import Home from '../src/componets/Home';
+
+function App() {
+  return (
+      <div className="App">
+    <Header/>
+    <Home/>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
